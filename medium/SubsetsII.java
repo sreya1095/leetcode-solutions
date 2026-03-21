37dd497ed31610aab9a76487e34600b185905740
sreya1095 <havappanoorsreya@gmail.com>
2026-03-21 17:00:05 +0530

    Added Subsets II (backtracking with duplicates) program

---
+++ medium/SubsetsII.java
@@ -0,0 +1,59 @@
+/*Time : O(2^n)
+Space : O(n)*/
+
+import java.util.*;
+
+public class SubsetsII {
+
+    public static List<List<Integer>> subsetsWithDup(int[] nums) {
+
+        Arrays.sort(nums);
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        backtrack(0, nums, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    public static void backtrack(int index, int[] nums, List<Integer> current, List<List<Integer>> result){
+
+        result.add(new ArrayList<>(current));
+
+        for(int i = index; i < nums.length; i++){
+
+            // skip duplicates
+            if(i > index && nums[i] == nums[i-1]){
+                continue;
+            }
+
+            current.add(nums[i]);
+
+            backtrack(i + 1, nums, current, result);
+
+            current.remove(current.size() - 1);
+        }
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter size:");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter elements:");
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
+
+        List<List<Integer>> result = subsetsWithDup(nums);
+
+        System.out.println("Subsets:");
+        for(int i = 0; i < result.size(); i++){
+            System.out.println(result.get(i));
+        }
+    }
+}
